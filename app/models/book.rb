1fd7bd8b2d1b78a072468cff91e74873ca32bcf3
jaketrent <trent.jake@gmail.com>
2014-11-28 14:07:06 -0700

    Don't require auth to list books; default order by complete_date

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ActiveRecord::Base
 
+  default_scope { order("complete_date desc") }
+
   validates :title, :author, :description, presence: true
   validates :cover_url, format: /\Ahttps?:\/\/[^.]+\..+\z/
   validates :review_url, :affiliate_url, format: /\Ahttps?:\/\/[^.]+\..+\z/, allow_blank: true
